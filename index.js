e4f188e437337bf515d758aef072a1062a7ffc0f
himayajawad19 <81642566+himayajawad19@users.noreply.github.com>
2021-08-13 17:58:53 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// When the user clicks the purchase button, render out
+// "Something went wrong, please try again" in the paragraph
+// that has the id="error".
+function error() {
+    let p="Something went wrong, please try again.s";
+    document.getElementById('error').innerHTML=p;
+    
+}
+
